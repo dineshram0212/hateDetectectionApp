e29561323018577323efe0941fa626f04298d7e8
Dinesh Ram R <70967093+dineshram0212@users.noreply.github.com>
2022-06-08 23:03:27 +0530

    Update hateDetection_app.py

--- hateDetection_app.py
+++ hateDetection_app.py
@@ -5,38 +5,42 @@ import pickle
 st.title("Hate Speech Detection")
 url = st.text_input('Enter URL')
 limit = st.slider("Select limit", min_value=1, max_value=100)
+if st.button('Enter'):
+    # fetch tweet from URL
+    username, tweets = getTweets(url, limit-1)
 
-# fetch tweet from URL
-username, tweets = getTweets(url, limit-1)
-
-# loading model
-with open('model_final.pkl', 'rb') as f:
+    # loading model
+    with open('model_final.pkl', 'rb') as f:
     model = pickle.load(f)
 
-# Making predictions
-prediction = predictdf(model, tweets)
+    # Making predictions
+    prediction = predictdf(model, tweets)
 
-for i in range(len(prediction)):
-    if prediction['label'][i]==0:
-            prediction['label'][i] = "No Hate Detected"
+    for i in range(len(prediction)):
+        if prediction['label'][i]==0:
+                prediction['label'][i] = "No Hate Detected"
+        else:
+            prediction['label'][i] = "Hate Detected"
+
+    # Display prediction
+    st.table(data=prediction[['Text', 'label']])
+
+    # Download predictions as CSV
+    @st.cache
+    def convert_df(df):
+        return df.to_csv().encode('utf-8')
+    csv = convert_df(prediction)
+
+    st.download_button("Download CSV", csv, "pred.csv")
+
+    # Text prediction
+    ip = st.text_input("Enter Text")
+    res = predictText(model, [ip])
+    if res==0:
+        st.subheader("No Hate Detected")
     else:
-        prediction['label'][i] = "Hate Detected"
+        st.subheader("Hate Detected")
 
-# Display prediction
-st.table(data=prediction[['Text', 'label']])
-
-# Download predictions as CSV
-@st.cache
-def convert_df(df):
-   return df.to_csv().encode('utf-8')
-csv = convert_df(prediction)
-
-st.download_button("Download CSV", csv, "pred.csv")
-
-# Text prediction
-ip = st.text_input("Enter Text")
-res = predictText(model, [ip])
-if res==0:
-    st.subheader("No Hate Detected")
 else:
-    st.subheader("Hate Detected")
+     st.write('Press enter')
+
